101c715eab3c479bd07e8d0bd4bc2ff421c5f45e
zakariem <zakariyem367@gmail.com>
2024-11-01 20:25:55 +0300

    Add Assets, Dependencies, and Plugins for Enhanced Functionality
    - Added new image and icon assets to the project, including various icons and images for UI elements.
    - Created Dart files (`icons.dart` and `images.dart`) to manage and reference these assets programmatically.
    - Updated the `pubspec.yaml` file to include new dependencies and dev_dependencies, such as `cloud_firestore`, `firebase_auth`, `firebase_storage`, `flutter_riverpod`, `google_sign_in`, `image_picker`, `path_provider`, `permission_handler`, `timeago`, `uuid`, `video_editor`, `video_player`, `flutter_staggered_grid_view`, and `google_nav_bar`.
    - Modified platform-specific files (`generated_plugin_registrant.cc` for Linux and Windows) to register new plugins and dependencies.
    - Updated the `pubspec.lock` file to reflect the latest versions of all dependencies and their transitive dependencies.
    - Added and configured various plugins and interfaces to support new functionalities, including file selection, Firebase services, and permission handling.

---
+++ lib/constants/icons.dart
@@ -0,0 +1,85 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsIconsAddAccount
+  /// assets/icons/add-account.png
+  static const String assetsIconsAddAccount = "assets/icons/add-account.png";
+
+  /// Assets for assetsIconsAhead
+  /// assets/icons/ahead.png
+  static const String assetsIconsAhead = "assets/icons/ahead.png";
+
+  /// Assets for assetsIconsBackward
+  /// assets/icons/backward.png
+  static const String assetsIconsBackward = "assets/icons/backward.png";
+
+  /// Assets for assetsIconsCamera
+  /// assets/icons/camera.png
+  static const String assetsIconsCamera = "assets/icons/camera.png";
+
+  /// Assets for assetsIconsCast
+  /// assets/icons/cast.png
+  static const String assetsIconsCast = "assets/icons/cast.png";
+
+  /// Assets for assetsIconsCreatePost
+  /// assets/icons/create-post.png
+  static const String assetsIconsCreatePost = "assets/icons/create-post.png";
+
+  /// Assets for assetsIconsGoLive
+  /// assets/icons/go-live.png
+  static const String assetsIconsGoLive = "assets/icons/go-live.png";
+
+  /// Assets for assetsIconsHelp
+  /// assets/icons/help.png
+  static const String assetsIconsHelp = "assets/icons/help.png";
+
+  /// Assets for assetsIconsNotification
+  /// assets/icons/notification.png
+  static const String assetsIconsNotification = "assets/icons/notification.png";
+
+  /// Assets for assetsIconsPause
+  /// assets/icons/pause.png
+  static const String assetsIconsPause = "assets/icons/pause.png";
+
+  /// Assets for assetsIconsPen
+  /// assets/icons/pen.png
+  static const String assetsIconsPen = "assets/icons/pen.png";
+
+  /// Assets for assetsIconsPlay
+  /// assets/icons/play.png
+  static const String assetsIconsPlay = "assets/icons/play.png";
+
+  /// Assets for assetsIconsPurchases
+  /// assets/icons/purchases.png
+  static const String assetsIconsPurchases = "assets/icons/purchases.png";
+
+  /// Assets for assetsIconsSearch
+  /// assets/icons/search.png
+  static const String assetsIconsSearch = "assets/icons/search.png";
+
+  /// Assets for assetsIconsSettings
+  /// assets/icons/settings.png
+  static const String assetsIconsSettings = "assets/icons/settings.png";
+
+  /// Assets for assetsIconsShortVideo
+  /// assets/icons/short-video.png
+  static const String assetsIconsShortVideo = "assets/icons/short-video.png";
+
+  /// Assets for assetsIconsTimeWatched
+  /// assets/icons/time-watched.png
+  static const String assetsIconsTimeWatched = "assets/icons/time-watched.png";
+
+  /// Assets for assetsIconsUpload
+  /// assets/icons/upload.png
+  static const String assetsIconsUpload = "assets/icons/upload.png";
+
+  /// Assets for assetsIconsYourChannel
+  /// assets/icons/your-channel.png
+  static const String assetsIconsYourChannel = "assets/icons/your-channel.png";
+
+  /// Assets for assetsIconsYourData
+  /// assets/icons/your-data.png
+  static const String assetsIconsYourData = "assets/icons/your-data.png";
+}
+
---
+++ lib/constants/images.dart
@@ -0,0 +1,46 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for assetsImagesFlutter background
+  /// assets/images/flutter background.png
+  static const String assetsImagesFlutterBackground =
+      "assets/images/flutter_background.png";
+
+  /// Assets for assetsImagesGo ahead final
+  /// assets/images/go ahead final.png
+  static const String assetsImagesGoAheadFinal =
+      "assets/images/go_ahead_final.png";
+
+  /// Assets for assetsImagesGoBackFinal
+  /// assets/images/go_back_final.png
+  static const String assetsImagesGoBackFinal =
+      "assets/images/go_back_final.png";
+
+  /// Assets for assetsImagesPause
+  /// assets/images/pause.png
+  static const String assetsImagesPause = "assets/images/pause.png";
+
+  /// Assets for assetsImagesPlay
+  /// assets/images/play.png
+  static const String assetsImagesPlay = "assets/images/play.png";
+
+  /// Assets for assetsImagesSigninwithgoogle
+  /// assets/images/signinwithgoogle.png
+  static const String assetsImagesSigninwithgoogle =
+      "assets/images/signinwithgoogle.png";
+
+  /// Assets for assetsImagesYoutube
+  /// assets/images/youtube.jpg
+  static const String assetsImagesYoutube = "assets/images/youtube.jpg";
+
+  /// Assets for assetsImagesYoutubeLogo
+  /// assets/images/youtube-logo.jpg
+  static const String assetsImagesYoutubeLogo =
+      "assets/images/youtube-logo.jpg";
+
+  /// Assets for assetsImagesYoutubeSignin
+  /// assets/images/youtube-signin.jpg
+  static const String assetsImagesYoutubeSignin =
+      "assets/images/youtube-signin.jpg";
+}
