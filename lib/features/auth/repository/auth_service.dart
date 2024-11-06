30e55dc65d204bbe827ce86740b8bbd98a76ecd2
zakariem <zakariyem367@gmail.com>
2024-11-06 07:34:14 +0300

    Refactor AuthService to handle Google sign-in errors and user cancellation
    - Added error handling and debug logging for Google sign-in process.
    - Modified `signInWithGoogle` method to return `UserCredential?` and handle cases where the user cancels the sign-in.
    - Simplified `AuthService` constructor by initializing FirebaseAuth and GoogleSignIn directly within the class.
    - Updated `authServiceProvider` to use the new `AuthService` constructor.

--- lib/features/auth/repository/auth_service.dart
+++ lib/features/auth/repository/auth_service.dart
@@ -1,30 +1,36 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-final authServiceProvider = Provider(
-  (ref) => AuthService(
-    auth: FirebaseAuth.instance,
-    googleSignIn: GoogleSignIn(),
-  ),
+final authServiceProvider = Provider<AuthService>(
+  (ref) => AuthService(),
 );
 
 class AuthService {
-  FirebaseAuth auth;
-  GoogleSignIn googleSignIn;
-  AuthService({
-    required this.auth,
-    required this.googleSignIn,
-  });
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
 
-  Future<UserCredential> signInWithGoogle() async {
-    final user = await googleSignIn.signIn();
-    final googleAuth = await user!.authentication;
-    final credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    );
-    return await auth.signInWithCredential(credential);
+  Future<UserCredential?> signInWithGoogle() async {
+    try {
+      final user = await _googleSignIn.signIn();
+      if (user == null) {
+        // Handle the case where the user cancels the sign-in
+        debugPrint('User cancelled Google sign-in');
+        return null;
+      }
+
+      final googleAuth = await user.authentication;
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      return await _auth.signInWithCredential(credential);
+    } catch (e) {
+      // Handle errors appropriately here (log or show a message)
+      debugPrint('Error during Google sign-in: $e');
+      return null;
+    }
   }
 }
