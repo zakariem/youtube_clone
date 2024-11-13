31517ada0ccee7ff2ce1795923e591aeb421198e
zakariem <zakariyem367@gmail.com>
2024-11-13 09:28:04 +0300

    Refactor MyChannelPage to use DefaultTabController for tabbed navigation
    - Introduced `DefaultTabController` to manage tabbed navigation within the `MyChannelPage`.
    - Updated the page layout to include a `TabBar` with 7 tabs: Home, Videos, Shorts, Playlists, Community, Channel, and About.
    - Enhanced the UI by wrapping the existing content within the `DefaultTabController` and `Scaffold`.
    - Adjusted the `TabBar` styling to ensure proper alignment and visual consistency.
    - Ensured that the `TabBar` is scrollable and includes appropriate padding and indicator styling.

--- lib/features/channel/my_channel/pages/my_channel_page.dart
+++ lib/features/channel/my_channel/pages/my_channel_page.dart
@@ -9,79 +9,110 @@ class MyChannelPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(24),
-          child: Center(
-            child: Column(
-              children: [
-                const CircleAvatar(
-                  radius: 34,
-                  backgroundColor: Colors.grey,
-                ),
-                const SizedBox(height: 6),
-                const Text(
-                  'Zakarie Mahad',
-                  style: TextStyle(
-                    fontSize: 26,
-                    fontWeight: FontWeight.bold,
+    return DefaultTabController(
+      length: 7,
+      child: Scaffold(
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(24),
+            child: Center(
+              child: Column(
+                children: [
+                  const CircleAvatar(
+                    radius: 34,
+                    backgroundColor: Colors.grey,
                   ),
-                ),
-                const SizedBox(height: 3),
-                RichText(
-                  text: const TextSpan(
-                    style: TextStyle(color: Colors.blueGrey),
-                    children: [
-                      TextSpan(text: 'zakarie@gmail.com  '),
-                      TextSpan(
-                        text: 'No Subscription  ',
-                      ),
-                      TextSpan(text: 'No Videos'),
-                    ],
+                  const SizedBox(height: 6),
+                  const Text(
+                    'Zakarie Mahad',
+                    style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 10),
-                const Text(
-                  'More About This Channel',
-                ),
-                const SizedBox(height: 10),
-                Row(
-                  children: [
-                    Expanded(
-                      flex: 3,
-                      child: Container(
-                        height: 41,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(7),
-                          color: softBlueGreyBackGround,
+                  const SizedBox(height: 3),
+                  RichText(
+                    text: const TextSpan(
+                      style: TextStyle(color: Colors.blueGrey),
+                      children: [
+                        TextSpan(text: 'zakarie@gmail.com  '),
+                        TextSpan(
+                          text: 'No Subscription  ',
                         ),
-                        child: TextButton(
-                          onPressed: () {},
-                          child: const Text(
-                            'Manage Videos',
-                            style: TextStyle(color: Colors.black),
+                        TextSpan(text: 'No Videos'),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  const Text(
+                    'More About This Channel',
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 3,
+                        child: Container(
+                          height: 41,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(7),
+                            color: softBlueGreyBackGround,
+                          ),
+                          child: TextButton(
+                            onPressed: () {},
+                            child: const Text(
+                              'Manage Videos',
+                              style: TextStyle(color: Colors.black),
+                            ),
                           ),
                         ),
                       ),
+                      Expanded(
+                        child: ImageButton(
+                          onPressed: () {},
+                          image: Iconst.assetsIconsPen,
+                          haveColor: true,
+                        ),
+                      ),
+                      Expanded(
+                        child: ImageButton(
+                          onPressed: () {},
+                          image: Iconst.assetsIconsTimeWatched,
+                          haveColor: true,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  const TabBar(
+                    isScrollable: true,
+                    labelStyle: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
                     ),
-                    Expanded(
-                      child: ImageButton(
-                        onPressed: () {},
-                        image: Iconst.assetsIconsPen,
-                        haveColor: true,
+                    unselectedLabelColor: Colors.black54,
+                    labelColor: Colors.black,
+                    indicator: BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(
+                          color: Colors.black,
+                          width: 3,
+                        ),
                       ),
                     ),
-                    Expanded(
-                      child: ImageButton(
-                        onPressed: () {},
-                        image: Iconst.assetsIconsTimeWatched,
-                        haveColor: true,
-                      ),
-                    ),
-                  ],
-                )
-              ],
+                    indicatorPadding: EdgeInsets.symmetric(horizontal: 8),
+                    tabs: [
+                      Tab(text: 'Home'),
+                      Tab(text: 'Videos'),
+                      Tab(text: 'Shorts'),
+                      Tab(text: 'Playlists'),
+                      Tab(text: 'Community'),
+                      Tab(text: 'Channel'),
+                      Tab(text: 'About'),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
