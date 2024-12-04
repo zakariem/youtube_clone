912ec976bb3dfb2dc6df13991f6727d5aff3c00d
zakariem <zakariyem367@gmail.com>
2024-12-04 07:57:34 +0300

    Add UserChannelPage with basic UI layout
    Implemented the UserChannelPage with a basic UI layout including a background image, user profile details, and a subscribe button. The page also displays a message indicating whether the user has videos or not.

---
+++ lib/features/channel/user_channel/pages/user_channel_page.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_clone/constants/images.dart';
+import 'package:youtube_clone/cores/widgets/flat_button.dart';
+
+class UserChannelPage extends StatefulWidget {
+  const UserChannelPage({super.key});
+
+  @override
+  State<UserChannelPage> createState() => _UserChannelPageState();
+}
+
+class _UserChannelPageState extends State<UserChannelPage> {
+  bool hasvideos = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Image.asset(
+              Images.assetsImagesFlutterBackground,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 40,
+                    backgroundColor: Colors.grey,
+                  ),
+                  SizedBox(
+                    width: 7,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Ahmed Ali',
+                        style: TextStyle(
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        '@ahmed-ai',
+                        style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.blueGrey,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 4,
+                      ),
+                      RichText(
+                        text: TextSpan(
+                          style: TextStyle(
+                            fontSize: 13,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.blueGrey,
+                          ),
+                          children: [
+                            TextSpan(text: 'No subscribers and '),
+                            TextSpan(text: 'No videos'),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: SizedBox(
+                width: double.infinity,
+                child: FlatButton(
+                    text: 'Subscribe', onPressed: () {}, colour: Colors.red),
+              ),
+            ),
+            hasvideos
+                ? Text('Videos')
+                : Center(
+                    child: Text('No videos'),
+                  ),
+          ],
+        ),
+      ),
+    );
+  }
+}
